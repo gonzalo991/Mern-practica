33dcac809eeb7838f0879f672d5989261fd57c3e
Gonzalo Araya <gonxaraya991@gmail.com>
2022-08-31 11:23:57 -0300

    produccion

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express();
 const path = require('path');
 const morgan = require('morgan');
-const { mongoose } = require('../src/database/db.database')
+const { mongoose } = require('./src/database/db.database')
 //require('dotenv').config({ path: './.env' });
 const PORT = process.env.PORT || 3000;
 
@@ -15,7 +15,7 @@ app.use(morgan('dev'));
 app.use(express.json()); //Método que me permite acceder a datos en formato json
 
 //Routes
-app.use('/api/task',require('./routes/tasks.routes'));
+app.use('/api/task',require('./src/routes/tasks.routes'));
 
 //Static files
 app.use(express.static(path.join(__dirname, 'public'))); //Dirección de la carpeta public
